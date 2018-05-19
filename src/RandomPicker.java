3a765bc88de67c6b02931d5f738ca7d1a2891235
Douglas Urner <dlu@canishe.com>
2018-05-18 23:14:39 -0700

    Prefix rows with date.

--- src/RandomPicker.java
+++ src/RandomPicker.java
@@ -1,3 +1,5 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,14 +14,13 @@ public class RandomPicker {
     for (int i = 1; i <= students; i++) {
       queue.add(i);
     }
-    //System.out.println(queue.toString());
 
     int spd = students / days;
     int excess = students % days;
 
     int[][] result = new int[days][];
 
-    // Build array.
+    // Build array - selecting students at random.
     for (int i = 0; i < days; i++) {
       int howmany = spd + (excess-- > 0 ? 1 : 0);
       result[i] = new int[howmany];
@@ -29,14 +30,39 @@ public class RandomPicker {
       }
     }
 
+    long epochTimeMilliSec = System.currentTimeMillis();
+    ZoneId tz = ZoneId.of("America/Vancouver");
+    //LocalDate today = LocalDate.now( z );
+
+    long next = epochTimeMilliSec;
+
     // Sort and print.
     for (int[] day : result) {
       Arrays.sort(day);
-      System.out.printf("%2d: ", day.length);
+      System.out.printf("%s: ", epochToDate(next, tz));
       for (int stu : day) {
           System.out.printf("%2d, ", stu);
       }
       System.out.println();
+      next = findNextClassDay(next, tz);
     }
+  }
+
+  private static long findNextClassDay(long last, ZoneId tz) {
+    final long twoDays = 2 * 24 * 60 * 60 * 1000;
+    long next = last + twoDays;
+    ZonedDateTime dt = Instant.ofEpochMilli(next).atZone(tz);
+
+    if (dt.getDayOfWeek() == DayOfWeek.SATURDAY ||
+        dt.getDayOfWeek() == DayOfWeek.SUNDAY) {
+      next += twoDays;
+    }
+
+    return next;
+  }
+
+  private static String epochToDate(long last, ZoneId tz) {
+    ZonedDateTime dt = Instant.ofEpochMilli(last).atZone(tz);
+    return dt.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
   }
 }
